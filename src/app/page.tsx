e84c7e6f0b412f4884f2fc16d035e53648768582
Yura <linetsky.yura@gmail.com>
2023-12-28 23:30:03 +0100

    fixed bug with home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Notification from '@/Components/Notification/page'
 import { Recorder } from '@/Components/Recorder/page'
 
-export const Home: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <main>
       <div className="container">
